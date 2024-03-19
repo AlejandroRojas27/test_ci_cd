0a1b75d27c95c2eac2cd63f5b31e01a276bb0e8f
Alejandro Rojas <rmae2727@gmail.com>
2024-03-19 17:50:31 -0600

    Add new branch feature/addtest

--- billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
+++ billing/target/generated-sources/annotations/com/paymentchain/billing/common/InvoiceResposeMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-03-19T15:23:09-0600",
-    comments = "version: 1.3.1.Final, compiler: javac, environment: Java 17.0.5 (Oracle Corporation)"
+    date = "2024-03-19T17:47:47-0600",
+    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 3.37.0.v20240206-1609, environment: Java 17.0.10 (Eclipse Adoptium)"
 )
 @Component
 public class InvoiceResposeMapperImpl implements InvoiceResposeMapper {
